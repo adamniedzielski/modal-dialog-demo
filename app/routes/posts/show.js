a829e1ffa2c6b7d6caed66df268e0a88c0f5de70
Adam Niedzielski <adamsunday@gmail.com>
2015-10-08 15:59:01 +0200

    Replace nested view with modal dialog

--- app/routes/posts/show.js
+++ app/routes/posts/show.js
@@ -5,5 +5,11 @@ export default Ember.Route.extend({
 
   model: function(params) {
     return this.get('postRepository').findById(params.post_id);
+  },
+
+  actions: {
+    goBackToList: function() {
+      this.transitionTo('posts');
+    }
   }
 });
